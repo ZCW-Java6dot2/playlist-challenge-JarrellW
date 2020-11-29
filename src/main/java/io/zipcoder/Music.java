935be996b89cfcbe7f796a061c056156f394cff9
Jay <73120876+JarrellW@users.noreply.github.com>
2020-11-28 21:06:35 -0500

    completed tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,38 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int upward = 0;
+        int downward = 0;
+
+        for (int i = startIndex; i < playList.length; i++) {
+            if (playList[i].equals(selection)){
+                break;
+            }
+            if (i + 1 > playList.length - 1) {
+                i = -1;
+            }
+            downward += 1;
+        }
+        for (int i = startIndex; i >= 0; i--) {
+            if (playList[i].equals(selection)) {
+                break;
+            }
+
+            if (i - 1 < 0) {
+                i = playList.length;
+            }
+            upward += 1;
+        }
+
+        if (downward > upward) {
+            return upward;
+        } else {
+            return downward;
+        }
     }
 }
